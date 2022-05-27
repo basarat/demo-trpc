0f8629643323fa5e61c5ccd12d3dc1afc157d3b2
basarat <basaratali@gmail.com>
2022-05-27 17:27:29 +1000

    final

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createTRPCClient } from '@trpc/client';
+import type { ApiRouter } from './api';
+
+const client = createTRPCClient<ApiRouter>({
+  url: 'api'
+});
 
 const Home: React.FC<{}> = () => {
-  return (
-    null
-  );
+  const [data, setData] = useState<null | { message: string }>(null);
+
+  const handleLoad = () => {
+    client.query('hello')
+      .then(res => {
+        setData({ message: res.payload });
+      });
+  };
+
+  return (<>
+    <button onClick={handleLoad}>Load Data</button>
+    {!!data &&
+      <pre>
+        Data: {data.message}
+      </pre>
+    }
+  </>);
 }
 
 export default Home;
--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,18 @@
-import { Router } from 'express';
+import * as trpc from '@trpc/server';
 
-export const api = Router();
+export const apiRouter =
+  trpc.router()
+    .query('hello', {
+      resolve: async (req) => {
+        return {
+          payload: 'Hi Fam',
+        };
+      }
+    });
+
+export type ApiRouter = typeof apiRouter;
+
+import * as trpcExpress from '@trpc/server/adapters/express';
+export const api = trpcExpress.createExpressMiddleware({
+  router: apiRouter,
+});
